5c946d1c6c3ae1c798b4c5b3eda71efbce0e232c
Beta-23 <alfred.dominguez@gmail.com>
2020-12-03 21:35:48 -0800

    Test py conditionals statements

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ print("Welcome to the Band Name Generator.")
 street = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print("Your band name could be " + street + " " + pet)
+
+n = 6
+if n > 2:
+  print("Larger than 2")
+else:
+  print("Smaller than 2")
+
+age = 13
+if age > 16:
+  print("Can drive")
+else:
+  print("Don't drive")
+
+  weather = "sunny"
+if weather == "rain":
+  print("bring umbrella")
+elif weather == "sunny":
+  print("bring sunglasses")
+elif weather == "snow":
+  print("bring gloves")
